432f05d25d5d02a998dd5b522c6113a268aa8f76
berko <berkomuz@gmail.com>
2020-03-05 11:13:49 +0300

    time denemesi yapıldı.

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-# This program prints Hello, world!
+# This program prints Hello, world! and then says hi hi hi after 5 seconds
+import time
+
 
 print('Hello, world!')
+
+time.sleep(5)
+
+print('hi hi hi')
